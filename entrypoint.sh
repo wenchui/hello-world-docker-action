7d98d0779cc822e9fe5ab28f5d2fb95b2df33fa9
Chenyt26 <2396164919@qq.com>
2022-01-25 10:10:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+wget https://obs-community.obs.cn-north-1.myhuaweicloud.com/obsutil/current/obsutil_linux_amd64.tar.gz
+tar -xzvf obsutil_linux_amd64.tar.gz
+cd obsutil_linux_amd64_5.3.4
+chmod 755 obsutil
+./obsutil config -i=${{ secrets.ACCESSKEY }} -k=${{ secrets.SECRETACCESSKEY }} -e=obs.cn-north-4.myhuaweicloud.com  
+cat /home/runner/.obsutilconfig
+./obsutil cp ${{ github.workspace }}/src obs://hdn-github-action/ -f -r
