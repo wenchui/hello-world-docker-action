a268fad127e1c0a86ad7ffc76f7a3a25256e0481
Chenyt26 <2396164919@qq.com>
2022-01-25 11:06:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ wget https://obs-community.obs.cn-north-1.myhuaweicloud.com/obsutil/current/obsu
 tar -xzvf obsutil_linux_amd64.tar.gz
 cd obsutil_linux_amd64_5.3.4
 chmod 777 obsutil
-source setup.sh /github/workspace/obsutil_linux_amd64_5.3.4/obsutil
-cat /etc/profile
+# source setup.sh /github/workspace/obsutil_linux_amd64_5.3.4/obsutil
+# cat /etc/profile
 ./github/workspace/obsutil_linux_amd64_5.3.4/obsutil config -i=${INPUT_ACCESS_KEY} -k=${INPUT_SECRET_KEY} -e=obs.cn-north-4.myhuaweicloud.com  
 ./github/workspace/obsutil_linux_amd64_5.3.4/obsutil cp ${{ github.workspace }}/README.md obs://hdn-github-action/ -f -r
