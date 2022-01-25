496df6caf637773e723251fcd6182a74fd8fa57e
Chenyt26 <2396164919@qq.com>
2022-01-25 11:13:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,6 @@ cd obsutil_linux_amd64_5.3.4
 chmod 777 obsutil
 # source setup.sh /github/workspace/obsutil_linux_amd64_5.3.4/obsutil
 # cat /etc/profile
-pwd
+ls -al
 ./obsutil config -i=${INPUT_ACCESS_KEY} -k=${INPUT_SECRET_KEY} -e=obs.cn-north-4.myhuaweicloud.com  
 ./obsutil cp ${{ github.workspace }}/README.md obs://hdn-github-action/ -f -r
