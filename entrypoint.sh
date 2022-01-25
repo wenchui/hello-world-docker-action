bdd871527ff54e31e228f89f4f2036e370a576b9
Chenyt26 <2396164919@qq.com>
2022-01-25 10:19:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/sh -l
 
+if [ -z "$INPUT_ACCESS_KEY" ]; then
+  echo "ak is not set. Quitting."
+  exit 1
+fi
+if [ -z "$INPUT_SECRET_KEY" ]; then
+  echo "sk is not set. Quitting."
+  exit 1
+fi
+
 wget https://obs-community.obs.cn-north-1.myhuaweicloud.com/obsutil/current/obsutil_linux_amd64.tar.gz
 tar -xzvf obsutil_linux_amd64.tar.gz
 cd obsutil_linux_amd64_5.3.4
 chmod 755 obsutil
-./obsutil config -i=${{ secrets.ACCESSKEY }} -k=${{ secrets.SECRETACCESSKEY }} -e=obs.cn-north-4.myhuaweicloud.com  
+./obsutil config -i=${INPUT_ACCESS_KEY} -k=${INPUT_SECRET_KEY} -e=obs.cn-north-4.myhuaweicloud.com  
 cat /home/runner/.obsutilconfig
 ./obsutil cp ${{ github.workspace }}/src obs://hdn-github-action/ -f -r
