a5a6554bd8733c55f1bf4c7fcf04e5ad8182082e
Chenyt26 <2396164919@qq.com>
2022-01-25 14:18:09 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,11 +14,11 @@ yum -y install wget
 
 wget https://obs-community.obs.cn-north-1.myhuaweicloud.com/obsutil/current/obsutil_linux_amd64.tar.gz
 tar -xzvf obsutil_linux_amd64.tar.gz
-cd obsutil_linux_amd64_5.3.4
-chmod 755 obsutil
+# cd obsutil_linux_amd64_5.3.4
+chmod 755 obsutil_linux_amd64_5.3.4/obsutil
 ls -al
-./obsutil config -i=${INPUT_ACCESS_KEY} -k=${INPUT_SECRET_KEY} -e=obs.cn-north-4.myhuaweicloud.com  
-./obsutil cp setup.sh obs://hdn-github-action/ -f -r
+./obsutil_linux_amd64_5.3.4/obsutil config -i=${INPUT_ACCESS_KEY} -k=${INPUT_SECRET_KEY} -e=obs.cn-north-4.myhuaweicloud.com  
+./obsutil_linux_amd64_5.3.4/obsutil cp README.md obs://hdn-github-action/ -f -r
 
 #cd /usr/local
 #wget https://obs-community.obs.cn-north-1.myhuaweicloud.com/obsutil/current/obsutil_linux_amd64.tar.gz
